aa48ccedd44201d578809d64662d4e8950e2805d
2Aura2 <Dato05.DJ@gmail.com>
2023-01-01 16:40:55 +0200

    Change

--- UserDB.py
+++ UserDB.py
@@ -8,7 +8,7 @@ class users:
         self.UserName = UserName
         self.Password = Password
         
-        conn=sqlite3.connect('userdata.db')
+        conn=sqlite3.connect('UserDB.db')
         print("Opened database successfully")
         str = "CREATE TABLE IF NOT EXISTS " + self.tablename + "(" + self.UserId + " " + "INTEGER PRIMARY KEY AUTOINCREMENT ,"
         str += " " + self.FullName + " TEXT    NOT NULL ,"
@@ -20,9 +20,15 @@ class users:
 
 
     def insert_user(self, FullName, UserName, Password):
-        conn = sqlite3.connect('userdata.db')
+        conn = sqlite3.connect('UserDB.db')
         str_insert = f"INSERT INTO {self.tablename} ({self.FullName},{self.UserName},{self.Password})VALUES('{FullName}','{UserName}','{Password}')"
         conn.execute(str_insert)
         conn.commit()
         conn.close()
         return "Record created successfully"
+
+
+
+
+
+u = users()
