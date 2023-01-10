15d58d6ea03594c7901bd8c55ee0f7eb5bbc2ca1
2Aura2 <Dato05.DJ@gmail.com>
2023-01-10 11:53:51 +0200

    change

---
+++ Server/Server.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+
+class Server(object):
+    def __init__(self,ip,port):
+        self.ip = ip
+        self.port = port
+        self.count = 0
+        self.running = True
+
+    def start(self):
+        try:
+            print(f"server starting up on ip {self.ip} port {self.port}")
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.bind((self.ip,self.port))
+            self.sock.listen(3)
+
+            while True:
+                print("Watinig for a new client")
+                clientSocket, client_addresses = self.socket.accept()
+                print("new client entered")
+                clientSocket.send("Hello, this is server".encode())
+                self.count += 1
+                print(self.count)
+                self.handleClient(clientSocket, self.count)
+        except socket.error as e:
+            print(e)
+
+    def handleClient(self,clientSock,current):
+        client_handler = threading.Thread(target=self.handle_client_connection, args=(clientSock, current,))
+        client_handler.start()
+
+    def handle_client_connection(self, client_socket, current):
+        not_crash = True
+        while self.running:
+            while not_crash:
+                try:
+                    server_data = client_socket.recv(1024).decode('utf-8')
+                except:
+                    print("error")
+                    not_crash = False
+                    break
