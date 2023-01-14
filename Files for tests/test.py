7b1616ef44e5cf51a1a4471cc3f9610788c5aa2b
2Aura2 <Dato05.DJ@gmail.com>
2023-01-14 17:44:51 +0200

    Change

--- Files for tests/test.py
+++ Files for tests/test.py
@@ -4,6 +4,9 @@ import sys
 sys.path.insert(1,'D://School Project//Python//DataBase_Codes')
 import UserDB
 
+server_data = UserDB.users().check_user_by_Username_and_Password("2Aura2","12345")
+print(server_data)
+
 
 
 
