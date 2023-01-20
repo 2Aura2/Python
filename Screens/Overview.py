b5f06a94e1f441238265c793efc6744925bfc2d0
2Aura2 <Dato05.DJ@gmail.com>
2023-01-20 16:56:15 +0200

    change

---
+++ Screens/Overview.py
@@ -0,0 +1,9 @@
+import tkinter
+
+
+class overview(tkinter.Toplevel):
+    def __init__(self,parent):
+        super().__init__(parent)
+        self.parent = parent
+        self.geometry("960x540")
+        self.title("Overview")
