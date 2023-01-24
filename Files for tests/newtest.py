06879f02587f7ffb8dbf2a0c43c6b943b0989a3d
2Aura2 <Dato05.DJ@gmail.com>
2023-01-24 14:45:55 +0200

    change

--- Files for tests/newtest.py
+++ Files for tests/newtest.py
@@ -7,14 +7,24 @@ def md5_hash(filename):
         f.close()
     return md5hash
 
-#print(md5_hash("Images\\784p9o.webp"))
+
+#print(md5_hash("Images\\Anti_Virus_BG.jpg"))
+#print(md5_hash("Images\\Background.jpg"))
+#print(md5_hash("Images\\Settings Button.png"))
+#print(md5_hash("Images\\thumb-1920-77840.jpg"))
 
 
 def malware_checker(PathOfFile):
     hash_malware_check = md5_hash(PathOfFile)
-    malware_hashes = open("virushash.txt")
+    
+    malware_hashes = open("Files for tests\\virushash.txt","r")
     malware_hashes_read = malware_hashes.read()
     malware_hashes.close()
-    print(malware_hashes_read)
+    
+    if malware_hashes_read.find(hash_malware_check) != -1:
+        return 1
+    else:
+        return 0
+    
 
-malware_checker("Images\\784p9o.webp")
+print(malware_checker("Images\\784p9o.webp"))
