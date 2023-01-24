c8ce8a21376b46683fb95ddaf58ce1387225f6a9
2Aura2 <Dato05.DJ@gmail.com>
2023-01-24 20:53:47 +0200

    change

--- Files for tests/disk path.py
+++ Files for tests/disk path.py
@@ -13,3 +13,15 @@ path_button = tk.Button(root, text="Select Disk Path", command=select_path)
 path_button.pack()
 
 root.mainloop()
+
+
+import os
+
+def scan_drive(drive):
+    for root, dirs, files in os.walk(drive):
+        for file in files:
+            file_path = os.path.join(root, file)
+            print("Scanning: ", file_path)
+            # Perform virus scan on the file here
+
+#scan_drive("C:\\")
