884cbf4cbb6e92abd2da311df486a20693df176d
2Aura2 <Dato05.DJ@gmail.com>
2023-01-24 17:33:25 +0200

    change

--- Files for tests/virus_in_folder.py
+++ Files for tests/virus_in_folder.py
@@ -1,6 +1,16 @@
 import os
 import hashlib
 
+#get hash of file
+def md5_hash(filename):
+    with open(filename,"rb")as f:
+        bytes = f.read()
+        md5hash = hashlib.md5(bytes).hexdigest()
+        f.close()
+    return md5hash
+
+#print(md5_hash("Images\\Background.jpg"))
+
 def check_folder_virus(folder_path):
     # Loop through all files in the specified folder
     for root, dirs, files in os.walk(folder_path):
@@ -19,7 +29,7 @@ def check_folder_virus(folder_path):
             # Check the file hash against a database of known virus hashes
             # This is where you would need to implement a database or external service
             # to check the file hash against a list of known virus hashes
-            known_virus_hashes = ["d5520a7f8caa7dd98e821f387cbf4f06","d72dc0459c292fe5b6d787cc5dd38447"]
+            known_virus_hashes = ["d5520a7f8caa7dd98e821f387cbf4f06","d72dc0459c292fe5b6d787cc5dd38447","0624e866088087793e074e98c5d90baa"]
             if file_hash in known_virus_hashes:
                 print(f"Virus detected in file: {file_path}")
     print("Scan complete. No more virus detected.")
