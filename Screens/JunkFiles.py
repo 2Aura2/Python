00595bbc92ed4e408d1f01cb6fbd153fd574953b
2Aura2 <Dato05.DJ@gmail.com>
2023-02-11 09:39:33 +0200

    change

--- Screens/JunkFiles.py
+++ Screens/JunkFiles.py
@@ -47,6 +47,27 @@ class Junk_Files_Screen(tkinter.Toplevel):
                 elif os.path.isdir(file_path):
                     shutil.rmtree(file_path)
 
+    def remove_temp_files(self):
+        temp_dir = os.environ["TEMP"]
+        if os.path.exists(temp_dir):
+            for root, dirs, files in os.walk(temp_dir):
+                for file in files:
+                    try:
+                        os.remove(os.path.join(root, file))
+                    except PermissionError:
+                        pass
+
+
+    def remove_browser_cache():
+        cache_dirs = [
+            os.path.expanduser(r"~\AppData\Local\Google\Chrome\User Data\Default\Cache"),
+            os.path.expanduser(r"~\AppData\Local\Mozilla\Firefox\Profiles"),
+            os.path.expanduser(r"~\AppData\Local\Microsoft\Edge\User Data\Default\Cache")
+        ]
+        for cache_dir in cache_dirs:
+            if os.path.exists(cache_dir):
+                shutil.rmtree(cache_dir)
+
 
 
     def open_overview_screen(self):
