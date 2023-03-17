af86990fc36411fbf7675db534035cc2b9ce7dd4
2Aura2 <Dato05.DJ@gmail.com>
2023-03-17 16:53:38 +0200

    change

--- Screens/History.py
+++ Screens/History.py
@@ -27,6 +27,7 @@ class History_Screen(tkinter.Toplevel):
 
     def create_gui(self):
         self.lbl_background = Label(self,bg="light gray",width=45,height=20).place(relx=0.2,rely=0.4,anchor='center')
+        self.lbl_text = Label(self,text="History screen allows\n you to see all your\n scans information",font=("ariel",18),bg="light gray").place(relx=0.2,rely=0.25,anchor='center')
         self.btn_previous_window = Button(self,text="Previous Window",font=("",18),width=16,bg="light gray",command=self.previous_window).place(relx=0.15,rely=0.9,anchor='center')
 
     def previous_window(self):
