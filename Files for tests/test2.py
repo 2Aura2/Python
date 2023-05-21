e1e34b4444ab331923a70cbf094f72681e24fa68
2Aura2 <dato05.DJ@gmail.com>
2023-05-21 17:01:32 +0300

    change

--- Files for tests/test2.py
+++ Files for tests/test2.py
@@ -1,4 +1,18 @@
+import tkinter as tk
+from tkinter import messagebox
 
+def show_message_box():
+    messagebox.showinfo("change", " Email changed successfully")
+
+# Create the main window
+window = tk.Tk()
+
+# Create a button widget
+button = tk.Button(window, text="Click me!", command=show_message_box)
+button.pack()
+
+# Start the Tkinter event loop
+window.mainloop()
         
 
 
